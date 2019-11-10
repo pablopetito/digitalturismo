4db817534ddb608cb7a043e63e89a09cce724470
Luciano <natielloluciano@gmail.com>
2019-11-10 11:03:58 -0300

    maqueta index

--- index.php
+++ index.php
@@ -5,37 +5,264 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/67f61afa3e.js" crossorigin="anonymous"></script>
     <title>digitalturismo</title>
 </head>
 <body>
  
-    
+    <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <header>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">DigitalTurismo</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">FAQ</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">DESTINOS</a>
+                        </li>
+                                               
+                      </ul>
+                      
+                        
+                        <button class="btn btn-outline-info my-2 my-sm-0" type="submit">INGRESAR</button>
+                     
+                    </div>
+                  </nav>
+                </header>
+        
+           </div>
+
+            <div class="col-12 carrusel">
+              <section class="carrusel">
+                 <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="images/bariloche.jpg" class="d-block w-100 img-fluid" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="images/foto.jpg" class="d-block w-100 img-fluid" alt="...">
+                      </div>
+                      
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+               </section>
+            </div> 
+
+            <section>
+              <div class="col-12">
+                <h2>
+                    Promociones
+                </h2> 
+              </div>
+            <div class="imagenes-contenedor d-flex flex-wrap">  
+              <div class="col-12 col-sm-6 col-md-4  flex-sm-shrink-0">
+                 <div class="card">
+                  <img src="images/paisaje.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+
+              </div>
+              <div class="col-12 col-sm-6 col-md-4  flex-sm-shrink-0">
+                 <div class="card">
+                  <img src="images/paisaje.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+
+              </div>
+              <div class="col-12 col-sm-6 col-md-4  flex-sm-shrink-0">
+                 <div class="card">
+                  <img src="images/paisaje.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+
+                </div> 
+
+              </div>
+               
+
+            </section>
+              
+            <section>
+              <div class="col-12">
+                <h2>
+                    Destacados
+                </h2> 
+              </div>
+          <div class="imagenes-contenedor d-flex flex-wrap">    
+                    <div class="col-12 col-sm-6 col-md-4  flex-sm-shrink-0">
+                      <div class="card">
+                        <img src="images/foto.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4  flex-sm-shrink-0">
+                      <div class="card">
+                        <img src="images/foto.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4  flex-sm-shrink-0">
+                      <div class="card">
+                        <img src="images/foto.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+
+                    </div>
+              </div>
+               
+
+            </section>
+
+            <section>
+              <div class="col-12">
+                <h2>
+                    Destinos
+                </h2> 
+              </div>
+              <div class="imagenes-contenedor d-flex flex-wrap">   
+                      <div class="col-12 col-sm-6 col-md-4  flex-sm-shrink-0">
+                        <div class="card">
+                          <img src="images/paisaje.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
+
+                      </div>
+                      <div class="col-12 col-sm-6 col-md-4  flex-sm-shrink-0">
+                        <div class="card">
+                          <img src="images/paisaje.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
+
+                      </div>
+                      <div class="col-12 col-sm-6 col-md-4  flex-sm-shrink-0">
+                        <div class="card">
+                          <img src="images/paisaje.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
+
+                      </div>
+              </div>
+               
+
+            </section>
+
+
+            <div class="col-12">
+              <footer>
+                    <div class="social d-flex flex-wrap">
+                        <div class="mision col-12">
+                            <h3>Nuestra Misión</h3>
+                            <h4 class="mt-3">DigitalTurismo busca fomentar el turismo en Argentina, para que crezca el sector, trayéndole a los usuarios los más lindos paisajes para visitar.</h4>
+                        </div>
+
+                            <div class="contacto mt-5 col-md-6">
+                                  <h3>Contacto</h3>
+                                  <form>
+                                      <div class="form-group">
+                                        <label for="exampleInputEmail1">Email</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                        
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="exampleFormControlTextarea1">Mensaje</label>
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                                      </div>
+                                      
+                                      <button type="submit" class="btn btn-info btn-block">Enviar</button>
+                                  </form>
+                              </div>
+
+
+                        <div class="red-social mt-5 col-md-6">
+                            <h3 class="mb-4">Visitanos</h3>
+                            <a class="a-iconos" href="#"><i class="fab fa-facebook-f icono-facebook mb-5"></i></a>
+                            <a class="a-iconos" href="#"><i class="fab fa-instagram mb-5"></i></a>
+                            <a class="a-iconos" href="#"><i class="fab fa-twitter mb-5"></i></a>
+                            
+                            
+
+                            
+                        </div>
+
+                        
+                </div>  
+              
+                <div class="copyright p-5 bg-dark">
+                      <span>Design DigitalTeam<i class="far fa-copyright"></i> 2020</span>
+                </div>
+              </footer>
+
+            </div>
+        
+        
+        
+        
+        
+        
         </div>
-      </nav>
+    
+    </div>
 
-
+    
 
 
 
