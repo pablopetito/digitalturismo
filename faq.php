4e3cc986f879a3ebf7d34996e6540cd3b2952792
Braian Soria <soriabriandaniel@gmail.com>
2019-11-19 21:41:56 -0300

    Primer commit faq y user

---
+++ faq.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+    <link rel="stylesheet" href="css/style-faq.css">
+    <script src="https://kit.fontawesome.com/562bf078ff.js" crossorigin="anonymous"></script>
+    <title>faq</title>
+</head>
+<body data-spy="scroll" data-target="#navbar-example">
+        <div class="container">
+
+
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Digital Turismo</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                  
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Contacto</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Mi cuenta
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Registrarme</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Iniciar sesion</a>
+                          </div>
+                        </li>
+                        
+                      </ul>
+                      <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                      </form>
+                    </div>
+                  </nav> 
+<h2>Preguntas Frecuentes</h2>    
+
+<div class="accordion md-accordion accordion-4" id="accordionEx2" role="tablist" aria-multiselectable="true">
+
+        <div class="card">
+      
+          <div class="card-header z-depth-1 teal lighten-4" role="tab" id="heading10">
+            <a data-toggle="collapse" data-parent="#accordionEx2" href="#collapse10" aria-expanded="true"
+              aria-controls="collapse10">
+              <h4 class="mb-0 black-text text-center font-weight-bold text-uppercase">
+                Que es DigitalTurismo
+              </h4>
+            </a>
+          </div>
+                <div id="collapse10" class="collapse show" role="tabpanel" aria-labelledby="heading10"
+            data-parent="#accordionEx2">
+            <div class="card-body rgba-teal-strong white-text">
+              <p>Digital turismo: es una agencia de turismo que opera 100% online. Los usuarios podrán contratar hoteles, autos,viajes, paquetes turísticos, cruceros; y elegir entre más de 100 líneas aéreas y entre más de 10.000 mil opciones en hoteles, casas y departamentos en alquiler, aprovechando las ofertas. Las personas que deseen contratar cualquiera de esos servicios de Digital Turismo, deberán generarse un usuario en el sitio web; podrán consultar el estado de la compra en “Digital Turismo” y una vez efectuado el viaje, podrán publicar en comentarios la experiencia de su viaje. Junto con Banco Galincia lanzaron "DIGITALQUIERO". Es una propuesta en la que permiten canjear puntos por pasajes; reservar hoteles en cualquier ciudad del mundo o acceder a paquetes turísticos. Además hay una opción de DTurismo Pymes con propuestas exclusivas.</p>
+      
+              <p>Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
+                sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+                farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them
+                accusamus labore.</p>
+            </div>
+          </div>
+        </div>      
+        <div class="card">
+          <div class="card-header z-depth-1 teal lighten-3" role="tab" id="heading11">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx2" href="#collapse11"
+              aria-expanded="false" aria-controls="collapse11">
+              <h4 class="mb-0 black-text text-center font-weight-bold text-uppercase">
+                Como Comprar
+              </h4>
+            </a>
+          </div>
+      
+          <div id="collapse11" class="collapse" role="tabpanel" aria-labelledby="heading11"
+            data-parent="#accordionEx2">
+            <div class="card-body rgba-teal-strong white-text">
+              <p>Te detallamos los pasos que deberás seguir para realizar tu compra: <br>
+
+                Hacé click sobre todos los productos que desees para conocer sus características . <br>
+                Subí al carrito los productos que te interesen comprar. <br>
+                Para iniciar el pago ingresá tu DNI y Clave Web. <br>
+                Completá los campos del Proceso de Pago. <br>
+                Podrás revisar tu pedido antes de confirmarlo y luego de aceptar los Términos y Condiciones hacé click en "Pagar". <br>
+                Las compras efectuadas se imputarán en tu Resumen de cuenta y podrás ver el comprobante de compra ingresando en la opción Mi Cuenta > Pedidos recientes. <br>
+                            
+                            ¿Qué tarjetas de crédito aceptan? <br>
+                Aceptamos Visa, Master Card y American Express y te ofrecemos múltiples opciones al momento de pagar tu reservación..</p>
+            </div>
+          </div>
+        </div>
+              <div class="card">
+                <div class="card-header z-depth-1 teal lighten-2" role="tab" id="heading12">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx2" href="#collapse12"
+              aria-expanded="false" aria-controls="collapse12">
+              <h4 class="mb-0 black-text text-center font-weight-bold text-uppercase">
+Condiciones de uso
+            </h4>
+            </a>
+          </div>
+                <div id="collapse12" class="collapse" role="tabpanel" aria-labelledby="heading12"
+            data-parent="#accordionEx2">
+            <div class="card-body rgba-teal-strong white-text">
+              <p>CONDICIONES ESPECIFICAS DE LAS OFERTAS CON PAGO ONLINE Estas ofertas son de pago online, se cobra el 100% del importe de la reserva en el momento de formalización de la misma. Cualquier modificación en una reserva presente, pasada o futura ya confirmada supondrá unos gastos de gestión de $400. La modificación deberá ser comunicada por e-mail al agente correspondiente, con 3 días laborables de antelación a la fecha de entrada. Se entenderá también como modificación la reducción en el número de habitaciones o de noches o el cambio de nombre del cliente y la modificación de las fechas del viaje. Asimismo, las cancelaciones supondrán unos gastos de gestión de ATRÁPALO de 1.300$. En cuanto a los gastos de cancelación por parte del proveedor de los servicios habrá que consultarlos en cualquier caso, aunque por norma general en las compras de "Escapadas en coche" las cancelaciones notificadas hasta 48 horas antes de la fecha de entrada no tendrán gastos por parte del hotel ( excepto condiciones de cancelación especiales indicadas en cada oferta ). Después de estas 48 horas, el hotel podrá facturar una noche tanto en concepto de cancelación de reserva como de no presentación ( no show ).
+                </p>
+            </div>
+          </div>
+        </div>
+              <div class="card">
+                <div class="card-header z-depth-1 teal lighten-1" role="tab" id="heading13">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx2" href="#collapse13"
+              aria-expanded="true" aria-controls="collapse13">
+              <h4 class="mb-0 black-text text-center font-weight-bold text-uppercase">
+                Problemas con la cuenta
+              </h4>
+            </a>
+          </div>
+                <div id="collapse13" class="collapse" role="tabpanel" aria-labelledby="heading13"
+            data-parent="#accordionEx2">
+            <div class="card-body rgba-teal-strong white-text">
+              <p>¿Cómo hago para cambiar mi email en la cuenta de Digital Turismo? Visita la página de tu Cuenta para editar tu información personal.
+                    ¿Cómo desvinculo la cuenta de mi cuenta de Facebook? ¿Ya no quieres ser nuestro amigo? Es fácil: inicia sesión en Digital Turismo, haz clic en el enlace "Iniciar sesión" y escribe el mismo email que usas para iniciar sesión en Facebook. Luego, en vez de añadir más información, haz clic en "Restablecer contraseña". Entonces podrás crear una nueva contraseña y desvincular tu cuenta de Facebook.
+                    ¿Cómo restablezco mi contraseña? En el inicio de sesión, haz clic en el enlace para restablecer la contraseña.</p>
+            </div>
+          </div>
+        </div>
+              <div class="card">
+                <div class="card-header z-depth-1 teal" role="tab" id="heading14">
+            <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx2" href="#collapse14"
+              aria-expanded="false" aria-controls="collapse14">
+              <h4 class="mb-0 black-text text-center font-weight-bold text-uppercase">
+                Contacto
+              </h4>
+            </a>
+          </div>
+                <div id="collapse14" class="collapse" role="tabpanel" aria-labelledby="heading14"
+            data-parent="#accordionEx2">
+            <div class="card-body rgba-teal-strong white-text">
+              <p><i class="fas fa-phone-alt"> Telefono</i> <h2> (011) 5263-7400</h2> 
+                <i class="far fa-clock">Horario de atencion </i> <h2>Lun a Vier 09:00hs a 17:30hs <br>
+                  Sab 09:00 a 13:00</h2>
+                  <i class="far fa-envelope">Mail</i> <h2>info@digitalturismo.com.ar</h2><br>
+              <iframe class="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.3313139805828!2d-58.38386198468745!3d-34.62106686592359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccb28ea8781cb%3A0x950feb519009506e!2sLima%201111%2C%20C1073AAW%20CABA!5e0!3m2!1ses-419!2sar!4v1573611372545!5m2!1ses-419!2sar" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe></p>
+            </div>
+          </div>
+        </div>
+      </div>
+<footer class="page-footer font-small blue pt-4">
+        <div class="container-fluid text-center text-md-left">
+                <div class="row">
+                  <div class="col-md-6 mt-md-0 mt-3">
+                    <h5 class="text-uppercase">Nuestra Mision</h5>
+              <p>DigitalTurismo busca fomentar el turismo en Argentina, para que crezca el sector, trayendole a los usuarios los mas lindos paisajes para visitar.</p>
+            </div>      
+            <div class="col-md-3 mb-md-0 mb-3">
+
+            <hr class="clearfix w-100 d-md-none pb-3">
+            <h5 class="text-uppercase font-weight-bold">Visitanos</h5>
+            <button><i class="fab fa-instagram"></i></button>
+            <button><i class="fab fa-twitter"></i></button>
+            <button><i class="fab fa-facebook-square"></i></button>
+            </div>
+         </div>      
+        </div>
+              <div class="footer-copyright text-center py-3">© 2019 Copyright:
+          <a href="#"> DigitalTurismo</a>
+        </div>      
+      </footer>
+    </div>
+ </div>
+</body>
+</html>
