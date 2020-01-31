2dba9346fc5860cbab2cc5c8fd207b5a2f35629a
Luciano <natielloluciano@gmail.com>
2020-01-30 23:43:59 -0300

    Avance con busqueda POO

--- digitalturismodb.sql
+++ digitalturismodb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-01-2020 a las 03:26:05
+-- Tiempo de generación: 31-01-2020 a las 03:42:49
 -- Versión del servidor: 10.3.16-MariaDB
 -- Versión de PHP: 7.3.6
 
@@ -35,7 +35,7 @@ CREATE TABLE `comentarios` (
   `comentario` text DEFAULT NULL,
   `puntuacion` tinyint(4) DEFAULT NULL,
   `fecha_publicacion` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -45,12 +45,24 @@ CREATE TABLE `comentarios` (
 
 CREATE TABLE `destinos` (
   `id_destino` int(11) NOT NULL,
-  `nombre_destino` varchar(50) NOT NULL,
-  `precio` decimal(10,4) NOT NULL,
+  `nombre_destino` varchar(100) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
   `promocion` tinyint(4) DEFAULT NULL,
-  `avatar_destino` varchar(50) NOT NULL,
-  `id_localidad` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `avatar_destino` varchar(100) NOT NULL,
+  `id_provincia` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `destinos`
+--
+
+INSERT INTO `destinos` (`id_destino`, `nombre_destino`, `precio`, `promocion`, `avatar_destino`, `id_provincia`) VALUES
+(1, 'BARILOCHE \"SKI\" 7 DIAS', '14500.00', 10, 'barilocheEsquiando.jpg', 15),
+(2, 'SALTA \"LA LINDA\"', '28900.00', 0, 'saltaMontañaAlpinista.jpg', 16),
+(3, 'MENDOZA CITY TOUR', '15000.00', 0, 'mendozaCiudad.jpg', 12),
+(4, 'CATARATAS EXTREM 10 DIAS', '8900.00', 10, 'cataratas-iguazu.jpg', 13),
+(5, 'MINA CLAVERO PURA AVENTURA', '10000.00', 0, 'paisaje1.jpg', 5),
+(6, 'SALTA ALPINISMO EXTREMO', '11000.00', 0, 'saltaMontañaAlpinista.jpg', 16);
 
 -- --------------------------------------------------------
 
@@ -62,7 +74,7 @@ CREATE TABLE `favoritos` (
   `id_favorito` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `id_destino` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -73,7 +85,7 @@ CREATE TABLE `favoritos` (
 CREATE TABLE `formas_pagos` (
   `id_forma_pago` int(11) NOT NULL,
   `nombre_pago` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -85,7 +97,7 @@ CREATE TABLE `imagenes_destinos` (
   `id_imagen_dest` int(11) NOT NULL,
   `imagen_dest` varchar(50) DEFAULT NULL,
   `id_destino` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -95,9 +107,9 @@ CREATE TABLE `imagenes_destinos` (
 
 CREATE TABLE `imagenes_usuarios` (
   `id_imagen_usu` int(11) NOT NULL,
-  `nombre_foto` varchar(50) NOT NULL,
+  `nombre_foto` varchar(100) NOT NULL,
   `id_usuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -109,7 +121,7 @@ CREATE TABLE `localidad` (
   `id_localidad` int(11) NOT NULL,
   `nombre_localidad` varchar(50) DEFAULT NULL,
   `id_provincia` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -120,7 +132,14 @@ CREATE TABLE `localidad` (
 CREATE TABLE `pais` (
   `id_pais` int(11) NOT NULL,
   `nombre_pais` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `pais`
+--
+
+INSERT INTO `pais` (`id_pais`, `nombre_pais`) VALUES
+(1, 'ARGENTINA');
 
 -- --------------------------------------------------------
 
@@ -132,7 +151,36 @@ CREATE TABLE `provincia` (
   `id_provincia` int(11) NOT NULL,
   `nombre_provincia` varchar(50) DEFAULT NULL,
   `id_pais` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `provincia`
+--
+
+INSERT INTO `provincia` (`id_provincia`, `nombre_provincia`, `id_pais`) VALUES
+(1, 'BUENOS AIRES', 1),
+(2, 'CATAMARCA', 1),
+(3, 'CHACO', 1),
+(4, 'CHUBUT', 1),
+(5, 'CÓRDOBA', 1),
+(6, 'CORRIENTES', 1),
+(7, 'ENTRE RÍOS', 1),
+(8, 'FORMOSA', 1),
+(9, 'JUJUY', 1),
+(10, 'LA PAMPA', 1),
+(11, 'LA RIOJA', 1),
+(12, 'MENDOZA', 1),
+(13, 'MISIONES', 1),
+(14, 'NEUQUÉN', 1),
+(15, 'RÍO NEGRO', 1),
+(16, 'SALTA', 1),
+(17, 'SAN JUAN', 1),
+(18, 'SAN LUIS', 1),
+(19, 'SANTA CRUZ', 1),
+(20, 'SANTA FÉ', 1),
+(21, 'SANTIAGO DEL ESTERO', 1),
+(22, 'TIERRA DEL FUEGO', 1),
+(23, 'TUCUMÁN', 1);
 
 -- --------------------------------------------------------
 
@@ -143,14 +191,21 @@ CREATE TABLE `provincia` (
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
   `nombre_usuario` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `facebook` varchar(50) DEFAULT NULL,
-  `instagram` varchar(50) DEFAULT NULL,
-  `twitter` varchar(50) DEFAULT NULL,
-  `avatar` varchar(50) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `facebook` varchar(100) DEFAULT NULL,
+  `instagram` varchar(100) DEFAULT NULL,
+  `twitter` varchar(100) DEFAULT NULL,
+  `avatar` varchar(100) DEFAULT NULL,
   `fecha_alta` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `email`, `password`, `facebook`, `instagram`, `twitter`, `avatar`, `fecha_alta`) VALUES
+(5, 'Luciano Natiello', 'natielloluciano@gmail.com', '$2y$10$JdS1WZnL9ztP.7xkThWw6.3KLcXkU6OYqtz5NJsJENWL8uEfuCDo2', 'https://www.facebook.com/', 'https://www.instagram.com/?hl=es-la', 'https://twitter.com/home', 'img_5e322c738f8ab.png', '2020-01-29 22:08:03');
 
 -- --------------------------------------------------------
 
@@ -164,7 +219,7 @@ CREATE TABLE `viajes_comprados` (
   `id_destino` int(11) NOT NULL,
   `fecha_compra` datetime DEFAULT NULL,
   `id_forma_pago` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Índices para tablas volcadas
@@ -183,7 +238,7 @@ ALTER TABLE `comentarios`
 --
 ALTER TABLE `destinos`
   ADD PRIMARY KEY (`id_destino`),
-  ADD KEY `id_localidad` (`id_localidad`);
+  ADD KEY `fk_provincia` (`id_provincia`);
 
 --
 -- Indices de la tabla `favoritos`
@@ -238,7 +293,8 @@ ALTER TABLE `provincia`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id_usuario`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `email_2` (`email`);
 
 --
 -- Indices de la tabla `viajes_comprados`
@@ -263,7 +319,7 @@ ALTER TABLE `comentarios`
 -- AUTO_INCREMENT de la tabla `destinos`
 --
 ALTER TABLE `destinos`
-  MODIFY `id_destino` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_destino` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `favoritos`
@@ -299,19 +355,19 @@ ALTER TABLE `localidad`
 -- AUTO_INCREMENT de la tabla `pais`
 --
 ALTER TABLE `pais`
-  MODIFY `id_pais` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pais` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `provincia`
 --
 ALTER TABLE `provincia`
-  MODIFY `id_provincia` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_provincia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `viajes_comprados`
@@ -334,7 +390,7 @@ ALTER TABLE `comentarios`
 -- Filtros para la tabla `destinos`
 --
 ALTER TABLE `destinos`
-  ADD CONSTRAINT `destinos_ibfk_2` FOREIGN KEY (`id_localidad`) REFERENCES `localidad` (`id_localidad`);
+  ADD CONSTRAINT `fk_provincia` FOREIGN KEY (`id_provincia`) REFERENCES `provincia` (`id_provincia`);
 
 --
 -- Filtros para la tabla `favoritos`
